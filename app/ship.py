9d47b6365770e7e40dc3058eb919df3da7aa68c5
Serhiy Prots <prots.sergiy7@gmail.com>
2018-11-28 11:00:41 +0200

    added ship file

---
+++ app/ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, screen):
+        """Initializing the ship and setting its starting position"""
+        self.screen = screen
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bpm')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Position each new ship at the bottom center of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draw the ship and its current location"""
+        self.screen.blit(self.image, self.rect)
