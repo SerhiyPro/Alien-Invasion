b5aa439ce70fbcd1949420012a30ec3e4ed8549e
Serhiy Prots <prots.sergiy7@gmail.com>
2018-11-28 12:35:28 +0200

    added ability to move for the ship

--- app/alien_invasion.py
+++ app/alien_invasion.py
@@ -15,11 +15,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Watch for keyboard and mouse events
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- app/ship.py
+++ app/ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initializing the ship and setting its starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,24 @@ class Ship:
         # Position each new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        # Update the ship's center value, not the rect
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship and its current location"""
