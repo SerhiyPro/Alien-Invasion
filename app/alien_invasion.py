25e3449d2125f039cfe02918eab8bddab030c7bf
Serhiy Prots <prots.sergiy7@gmail.com>
2018-11-28 15:03:58 +0200

    added ability to shoot

--- app/alien_invasion.py
+++ app/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -17,11 +18,15 @@ def run_game():
     # Make a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in
+    bullets = Group()
+
     # Watch for keyboard and mouse events
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
